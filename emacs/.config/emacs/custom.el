36bf6352b86cb1427226e4275022a89a5a2e1223
= <jgeddes@turing.ac.uk>
2023-03-03 13:34:03 +0000

    Split out emacs custom variables into separate file

---
+++ emacs/.config/emacs/custom.el
@@ -0,0 +1,22 @@
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default))
+ '(package-selected-packages
+   '(paredit ess quarto-mode request poly-markdown polymode magit color-theme-sanityinc-solarized use-package))
+ '(warning-suppress-types '((comp))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Fira Code"))))
+ '(font-lock-constant-face ((t (:foreground "medium sea green"))))
+ '(fringe ((t (:foreground "#93a1a1"))))
+ '(org-level-1 ((t (:inherit outline-1 :extend nil :weight bold))))
+ '(tuareg-font-lock-governing-face ((t (:weight bold)))))
