e8d010aed5ee9f35d31c6aa60158d196757507f5
James Geddes <jgeddes@turing.ac.uk>
2023-03-06 21:28:35 +0000

    Added more packages to emacs config.

--- emacs/.config/emacs/custom.el
+++ emacs/.config/emacs/custom.el
@@ -7,7 +7,7 @@
  '(custom-safe-themes
    '("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default))
  '(package-selected-packages
-   '(company paredit ess quarto-mode request poly-markdown polymode magit color-theme-sanityinc-solarized use-package))
+   '(org poly-org company paredit ess quarto-mode request poly-markdown polymode magit color-theme-sanityinc-solarized use-package))
  '(warning-suppress-types '((comp))))
 
 (custom-set-faces
@@ -17,6 +17,6 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Fira Code"))))
  '(font-lock-constant-face ((t (:foreground "medium sea green"))))
- '(fringe ((t (:foreground "#93a1a1"))))
+ '(fringe ((t (:background nil))))
  '(org-level-1 ((t (:inherit outline-1 :extend nil :weight bold))))
  '(tuareg-font-lock-governing-face ((t (:weight bold)))))
